ee6b079888ab3b1c5a7342105b51dd3a906bd087
Samibgh <78345903+Samibgh@users.noreply.github.com>
2023-03-06 16:20:50 +0100

    s

--- format.py
+++ format.py
@@ -1,24 +1,3 @@
-import pandas as pd 
-
-#observation du fichier log ayant la plus petite difficulte 
-log3 = pd.read_csv("log_fw_3.csv", header=0)
-
-firstlog = pd.read_csv("firstlog.csv", sep=";")
-#print(firstlog.head())
-
-firstlog = firstlog.drop(firstlog.iloc[:,7:],axis = 1)
-
-firstlog.columns = ["datetime", "ipsrc", "ipdst","proto","dstport", "action", "policyid"]
-
-print(firstlog.head())
-
-
-print(firstlog.info())
-
-
-
-##########################################################################
-
 from connexion import connexion
 import pandas as pd
 
@@ -29,5 +8,12 @@ a = "SELECT * FROM fw"
 df = pd.read_sql(a,con)
 
 print(df.head())
-    
 
+
+print(df.info())
+
+print(df.shape)
+
+print(df.action.value_counts())
+
+print(df.isna().sum())
