dcbc887ae86d3648c13328e6f905734cde4ac94a
Robin_Ponson <83652394+Rocksorr@users.noreply.github.com>
2023-03-07 10:54:06 +0100

    Update format.py

--- format.py
+++ format.py
@@ -6,14 +6,3 @@ con = connexion("localhost", "root", "", "securite")
 a = "SELECT * FROM fw"
 
 df = pd.read_sql(a,con)
-
-print(df.head())
-
-
-print(df.info())
-
-print(df.shape)
-
-print(df.action.value_counts())
-
-print(df.isna().sum())
